5976d181edf34d397d8c1d591b506553b668a050
MATTHEWXT <aspoolup@gmail.com>
2024-12-04 03:50:22 +0500

    Create Domain entities

---
+++ ToDo.API/Domain/Entities/TaskCategory.cs
@@ -0,0 +1,20 @@
+﻿using ToDo.API.Domain.Core.Models;
+
+namespace ToDo.API.Domain.Entities
+{
+    public class TaskCategory : BaseEntity
+    {
+        public string Name { get; set; }
+        public List<TaskItem> Items { get; set; } = new List<TaskItem>();
+
+        public TaskCategory(string name) {
+            Name = name;
+        }
+
+        public void Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+                throw new ArgumentException("Name of TaskCategory is required.");
+        }
+    }
+}
---
+++ ToDo.API/Domain/Entities/TaskItem.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using ToDo.API.Domain.Core.Models;
+
+namespace ToDo.API.Domain.Entities
+{
+    public class TaskItem : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public Guid CategoryId { get; set; }
+        public TaskCategory Category { get; set; }
+
+        public TaskItem(string name, string description) {
+            Name = name;
+            Description = description;
+        }
+
+        public void Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+                Name = "My task";
+            if(string.IsNullOrWhiteSpace(Description))
+                throw new ArgumentException("Description is required.");
+        }
+    }
+}
