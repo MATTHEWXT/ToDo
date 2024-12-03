0a45cfa3ffaec785c0b3704b45dfaa156dec4e85
MATTHEWXT <aspoolup@gmail.com>
2024-12-04 03:51:29 +0500

    Create Domain core

---
+++ ToDo.API/Domain/Core/Models/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDo.API.Domain.Core.Models
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
