47c44b3365886bc2880098a5cada7914cd0348d2
MATTHEWXT <aspoolup@gmail.com>
2024-12-04 03:48:52 +0500

    Create jRpc API

---
+++ ToDo.API/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using ToDo.API.Infrastructure.Data;
+using ToDo.API.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddGrpc();
+
+
+var app = builder.Build();
+
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
